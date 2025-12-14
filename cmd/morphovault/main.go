70c0a6815fb65f0b1720eee426d673908ca38c6f
Muski <143282825+muskitma@users.noreply.github.com>
2025-12-14 23:13:46 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/morphovault/main.go
@@ -0,0 +1,23 @@
+// cmd/morphovault/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "morphovault/internal/morphovault"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := morphovault.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
